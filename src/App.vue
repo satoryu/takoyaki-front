6adc50745499808dd1bd89718358202323d350f6
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-04-01 16:43:15 +0900

    Install Vuetify and scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        <h1>{{ greeting }} {{ name }}!</h1>
-    </div>
+    <v-app>
+        <v-toolbar app></v-toolbar>
+        <v-content>
+            <v-container fluid>
+                {{ greetingMessage }}
+            </v-container>
+        </v-content>
+        <v-footer app></v-footer>
+    </v-app>
 </template>
 
 <script>
+import Vue from 'vue';
+
+import '@mdi/font/css/materialdesignicons.css'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  iconfont: 'mdi'
+})
+
 export default {
     data: function() {
         return {
             greeting: 'Hello,',
             name: 'Vue Component'
+        }
+    },
+    computed: {
+        greetingMessage() {
+            return `${this.greeting} ${this.name}!`
         }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import Vue from 'vue';
-
-import '@mdi/font/css/materialdesignicons.css'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
-
-Vue.use(Vuetify, {
-  iconfont: 'mdi'
-})
-
 import App from './App.vue';
 
 new Vue({
