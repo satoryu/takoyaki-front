9edf71de2ba9a983a6fdd24ccfe9f1a3196eece8
Tatsuya Sato <satoryu.1981@gmail.com>
2019-05-07 19:06:54 +0900

    Move importing store to top index.js

--- test/components/Content.test.js
+++ test/components/Content.test.js
@@ -1,10 +1,51 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(Vuetify)
+
 import Content from '../../src/components/Content.vue'
 
 import axios from 'axios'
-jest.mock('axios')
+import { exportAllDeclaration } from '@babel/types';
+const mockPost = jest.fn()
+jest.mock('axios', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            post: mockPost
+        }
+    })
+})
+
 
 describe('Content.vue', () => {
-    it('should call postTweet API', () => {
-        expect('foo').toEqual('foo')
+    let getters
+
+    describe('Before login', () => {
+        beforeEach(() => {
+            getters = {
+                check: jest.fn().mockImplementation(() => true ),
+                userId: jest.fn()
+            }
+        })
+
+       it('should call postTweet API', () => {
+            let store = new Vuex.Store({
+                modules: {
+                    auth: {
+                        namespaced: true,
+                        getters
+                    }
+                }
+            })
+
+            shallowMount(Content, {
+                store, localVue
+            })
+
+            expect(getters.check).toHaveBeenCalled()
+        })
     })
 })
